c361cc188e7ec6252b350fa99b7b2b564aa717e1
joshuaw001 <47309531+joshuaw001@users.noreply.github.com>
2019-08-23 15:57:03 -0500

    Update database.py

--- database.py
+++ database.py
@@ -1,2 +1,13 @@
+import pymysql as sql
 
-import pymysql
+if not users_db and not players_cur:
+    users_db = sql.connect("localhost","admin","p@ssword","data/users")
+    users_cur = sql.cursor()
+
+if not players_db and not players_cur:
+    players_db = sql.connect("localhost","admin","p@ssword","data/players")
+    players_cur = sql.cursor()
+
+# create tables if not exists
+
+user_cur.execute("CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT, level RANKS);")
