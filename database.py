3424bdacf5b5d3eb9795d30fb15beca0defd7556
joshuaw001 <47309531+joshuaw001@users.noreply.github.com>
2019-08-23 16:02:05 -0500

    Update database.py

--- database.py
+++ database.py
@@ -9,5 +9,5 @@ if not players_db and not players_cur:
     players_cur = sql.cursor()
 
 # create tables if not exists
-
+user_cur.execute("ENUM RANKS(beginner=0,starter=1,rookie=2,journeyman=3,master=4,expert=5,king=6,`king2`=7,`king3`=8);")
 user_cur.execute("CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT, level RANKS);")
