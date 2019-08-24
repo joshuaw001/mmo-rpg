c0ad70f69799cdc4f3d42c07197895d3b476ddf0
joshuaw001 <47309531+joshuaw001@users.noreply.github.com>
2019-08-24 16:06:19 -0500

    Update database.py

--- database.py
+++ database.py
@@ -12,3 +12,7 @@ if not players_db and not players_cur:
 user_cur.execute("ENUM RANKS(beginner=0,starter=1,rookie=2,journeyman=3,master=4,expert=5,king=6,`king2`=7,`king3`=8);")
 user_cur.execute("CREATE TABLE IF NOT EXISTS users (id INT,username TEXT, password TEXT, level RANKS);")
 user_cur.execute("INSERT INTO users VALUES(1,'admin','p@ssword',`king3`);")
+user_cur.fetchone()
+
+player_cur.execute("ENUM RACES(pixel,square,circle,triangle,npc);")
+player_cur.execute("CREATE TABLE IF NOT EXISTS players (id INT, user TEXT,health INT,xp INT(5),items INT);"
